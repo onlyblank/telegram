13193529b79177f040410f15464e3e24f97b37b8
doritosxxx <georgyvav@ya.ru>
2023-01-21 18:12:10 +0300

    Create telegram env file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
 import { load } from 'ts-dotenv';
 
+process.env.API_URL = 'http://cms:1337/api';
+
 export const config = load({
     PORT: Number,
+    TG_BOT_TOKEN: String,
+
+    API_URL: String,
+    STRAPI_IDENTIFIER: String,
+    STRAPI_PASSWORD: String,
 });
