04c66a06786c2d164ae1733ac308ccf26d7fb21c
doritosxxx <georgyvav@ya.ru>
2023-04-01 23:32:24 +0300

    Update assignments command

--- src/commands/assignments.command.ts
+++ src/commands/assignments.command.ts
@@ -1,16 +1,33 @@
+import { InlineKeyboard } from "grammy";
+import { GET } from "src/@types/resources";
 import { getAssignedTests } from "../queries/test";
 import { Command } from "./types";
+
+function createKeyboard(tests: GET.Test[]): InlineKeyboard {
+    const keyboard = new InlineKeyboard();
+
+    for(const test of tests) {
+        keyboard.text(test.title).row();
+    }
+
+    return keyboard;
+}
+
 
 const middleware: Command['middleware'] = async (ctx) => {
     const username = ctx.from?.username;
 
     const tests = await getAssignedTests(username!);
 
-    const message = tests.length 
-        ? tests.map(({ title }, i) => `${i + 1}) ${title}`).join('\n')
-        : "Нет назначенных тестов";
+    const message = tests.length !== 0 
+        ? "Список доступных тестов:" 
+        : "Нет доступных тестов";
 
-    return await ctx.reply(message);
+    const options = tests.length !== 0 ? {
+        reply_markup: createKeyboard(tests)
+    } : undefined;
+
+    return await ctx.reply(message, options);
 }
 
 export const assignmentsCommand: Command = {
