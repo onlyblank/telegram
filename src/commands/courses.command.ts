09f573aa586e4648e65cf92d16ba5378940800d1
doritosxxx <georgyvav@ya.ru>
2023-04-06 14:55:30 +0300

    Add course query

--- src/commands/courses.command.ts
+++ src/commands/courses.command.ts
@@ -1,9 +1,11 @@
-import { getCoursesByUsername } from "../queries/course";
+import { getUserId } from "../queries/user";
+import { getAssignedCourses } from "../queries/course";
 import { Command } from "./types";
 
 const middleware: Command['middleware'] = async (ctx) => {
     const username = ctx.from?.username!;
-    const courses = await getCoursesByUsername(username);
+    const userId = await getUserId(username);
+    const courses = !userId ? [] : await getAssignedCourses(userId);
     const message = courses.length === 0 
         ? "У вас нет назначенных курсов"
         : courses.map(({ name }, i) => `${i + 1}) ${name}`).join('\n')
--- src/queries/course.ts
+++ src/queries/course.ts
@@ -2,8 +2,19 @@ import { GET } from "src/@types/resources";
 import * as request from '../request'
 
 
-export function getCoursesByUsername(username: string): Promise<GET.Course[]> {
+export function getAssignedCourses(userId: number): Promise<GET.Course[]> {
     return request
-        .get<{ assigned_courses: GET.Course[] }[]>(`/users?populate=assigned_courses&filters[telegram_username][$eq]=${username}`)
-        .then(({ data }) => data.length === 0 ? [] : data[0].assigned_courses);
+        .get<{ assigned_courses: GET.Course[] }>(`/users/${userId}?populate=assigned_courses`)
+        .then(({ data }) => data.assigned_courses);
+}
+
+interface Coruses { 
+    assigned_courses: GET.Course[];
+    owned_courses: GET.Course[];
+}
+
+export function getAssignedAndOwnedCourses(userId: number): Promise<Coruses> {
+    return request
+        .get<Coruses>(`/users/${userId}?populate=assigned_courses&populate=owned_courses`)
+        .then(({ data }) => data);
 }
